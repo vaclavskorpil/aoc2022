d443014613c1902104ea83025c0d2d7aeafd2f21
Václav Škorpil <vaclav.skorpil@cleverfarm.ag>
2022-12-06 07:43:22 +0100

    Prepare day 7

---
+++ src/main/kotlin/Day07.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+    fun part1(input: String) = input
+
+    fun part2(input: String) = input
+
+    val input = readFile("Day07_input")
+
+    println(part1(input))
+    println(part2(input))
+}
